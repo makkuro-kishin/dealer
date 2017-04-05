ad06dda7c5180841e54836485d25ae986fc47923
Yurii Kucherenko <yurii.kucherenko@globallogic.com>
2017-04-05 11:26:17 +0300

    Refactored for PEP requirements

--- dealer.py
+++ dealer.py
@@ -4,10 +4,10 @@ try:
     import sys
     import modules.controller
 except Exception as import_error:
-    print("[FAIL] Unable to start controller!\n", import_error, "\nAborting...", sep='')
+    print('[FAIL] Unable to start controller!\n', import_error, '\nAborting...', sep='')
     raise SystemExit
 else:
-    print("Welcome to Dealer.")
+    print('Welcome to Dealer.')
 
 
 modules.controller.main()
--- modules/controller.py
+++ modules/controller.py
@@ -4,18 +4,18 @@ try:
     import modules.storage
     import modules.interaction
 except Exception as import_error:
-    print("[FAIL] Unable to import modules!\n", import_error, "\nAborting...", sep='')
+    print('[FAIL] Unable to import modules!\n', import_error, '\nAborting...', sep='')
     raise SystemExit
 
 
 def get_primary_selection():
     modules.interaction.show_primary_menu()
     primary_action = modules.interaction.get_action()
-    if primary_action == "1":
-        print("TODO")
-    elif primary_action == "2":
+    if primary_action == '1':
+        print('TODO')
+    elif primary_action == '2':
         get_secondary_selection()
-    elif primary_action == "3":
+    elif primary_action == '3':
         raise SystemExit
     else:
         modules.interaction.print_error()
@@ -25,13 +25,13 @@ def get_primary_selection():
 def get_secondary_selection():
     modules.interaction.show_secondary_menu()
     secondary_action = modules.interaction.get_action()
-    if secondary_action == "1":
-        print("TODO")
-    elif secondary_action == "2":
-        print("TODO")
-    elif secondary_action == "3":
-        print("TODO")
-    elif secondary_action == "4":
+    if secondary_action == '1':
+        print('TODO')
+    elif secondary_action == '2':
+        print('TODO')
+    elif secondary_action == '3':
+        print('TODO')
+    elif secondary_action == '4':
         raise SystemExit
     else:
         modules.interaction.print_error()
